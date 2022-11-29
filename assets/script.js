4fc16dce9bdb28399b30435824e4d502678a452a
NatJoshYoyo <joshuaobrien28@yahoo.com>
2022-11-28 23:19:29 -0500

    Fixed JS, Added Colors for past, futur, added Images folder

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,61 @@
-(function () {
+$(function () {
+    //Global var
     var currentTime = dayjs();
     var blockNumber = $(".time-block").map(function () { return (this.id) }).toArray();
 
+    //Save Feature
     $(".saveBtn").click(saveEvent);
 
     function saveEvent() {
         var blockHour = $(this).parent().attr("id");
         var desc = $(this).siblings(".description").val();
-        var blockHourDisplay = $(this).siblings('hour').text();
+        var blockHourDisplay = $(this).siblings(".hour").text();
 
         localStorage.setItem(blockHour, desc);
 
-        alert("Saved event to " + blockHourDisplay + "!/n" + dec);
+        alert("Saved event to " + blockHourDisplay + "!\n" + desc);
+        //Alert to notify save
     }
 
     checkTime();
     setInterval(checkTime, 1000);
 
+    //Checks the current time to update the color coding of time block
     function checkTime() {
         for (var i = 0; i < blockNumber.length; i++) {
-            var blockNumberEL = document.getElementById(blockNumber[i]);
-            var currentHour = dayjs().get('hour');
-            blockNumberEL.classList.remove("past", "present", "future");
-
-            if (blockNumber[i] < currentHour) {
-                blockNumberEL.classList.add('past');
-            } else if (blockNumber[i] == currentHour) {
-                blockNumberEL.classList.add("present");
-            } else {
-                blockNumberEL.classList.add("future");
-            }
+          var blockNumberEl = document.getElementById(blockNumber[i]);
+          var currentHour = dayjs().get("hour");
+          blockNumberEl.classList.remove("past", "present", "future");
+    
+          // Coded if Time is current or passed
+          if (blockNumber[i] < currentHour) {
+            blockNumberEl.classList.add("past");
+          } else if (blockNumber[i] == currentHour) {
+            blockNumberEl.classList.add("present");
+          } else {
+            blockNumberEl.classList.add("future");
+          }
         }
     };
-
+    //Loading event back in
     loadEvent();
     function loadEvent() {
-        var eventlist = [];
-
+        var eventList = [];
         for (var i = 0; i < blockNumber.length; i++) {
-            eventlist.push(localStorage.getItem(blockNumber[i]));
+            eventList.push(localStorage.getItem(blockNumber[i]));
         }
-
         $(".description").each(function () {
-            var taskNumber = ($(this).closets(".time-block").attr("id")) - 9;
-            $(this).text(eventlist[taskNumber]);
+            var taskNumber = ($(this).closest(".time-block").attr("id")) - 8;
+            $(this).text(eventList[taskNumber]);
         })
     };
 
-    Date();
+    //Show time on page
+    date();
     setInterval(date, 1000);
-
+    //Format
     function date() {
         var currentTime = dayjs();
         $("#currentDay").text(currentTime.format("[It is] MMMM D, YYYY [ at ]hh:mm:ssa"));
     }
-
 });
